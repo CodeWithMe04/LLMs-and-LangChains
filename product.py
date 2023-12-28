f28a33ec38c3d280ba2fd5258884dfc196644b97
Arin <rejeevkumar04@gmail.com>
2023-12-28 15:47:51 +0530

    Adding verbose

--- product.py
+++ product.py
@@ -12,7 +12,7 @@ This code will tell you that we can also make AI with LangChain that provides LL
 qproduct = PromptTemplate.from_template("What is the name of ecommerce store that sells {product}")
 llm = OpenAI(temperature=0.3, openai_api_key = "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")
 
-finding = LLMChain(llm=llm, prompt=qproduct)
+finding = LLMChain(llm=llm, prompt=qproduct, verbose=True)
 name = "MacBook"
 answer = finding.run(name)
 print(answer)
